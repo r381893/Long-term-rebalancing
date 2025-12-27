f319f51f2804d3dcf31f24ac5438ac18862ee9cc
r381893 <focus36473647@gmail.com>
2025-12-27 13:03:43 +0800

    加入防快取 meta 標籤

--- app.py
+++ app.py
@@ -19,6 +19,13 @@ st.set_page_config(
     layout="wide"
 )
 
+# 防止瀏覽器快取
+st.markdown('''
+<meta http-equiv="Cache-Control" content="no-cache, no-store, must-revalidate">
+<meta http-equiv="Pragma" content="no-cache">
+<meta http-equiv="Expires" content="0">
+''', unsafe_allow_html=True)
+
 # Premium Dark Theme CSS (完整版 from dashboard.html)
 st.markdown("""
 <style>
